9e73e0d86dabfce5af8ebbc357c61fe2e446d593
Pedro Rego <pedro.rego@stark.dk>
2024-08-13 11:23:53 +0100

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import { StyledEngineProvider, ThemeProvider } from "@mui/material/styles";
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter } from "react-router-dom";
-import AppRoutes from "./routes";
-import { theme } from "./theme/theme";
-
 function App() {
-  const queryClient = new QueryClient();
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <StyledEngineProvider injectFirst>
-            <ThemeProvider theme={theme}>
-              <AppRoutes />
-            </ThemeProvider>
-          </StyledEngineProvider>
-        </LocalizationProvider>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      teste
+    </div>
   );
 }
 
